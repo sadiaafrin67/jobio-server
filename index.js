4fe5f6d9ee1b7411d3e001e43a61f5868a1ba3a9
sadiaafrin67 <sa540276@gmail.com>
2023-11-08 15:28:39 +0600

    add my bid details code

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
 const express = require("express");
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const port = process.env.PORT || 5000;
-
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const app = express();
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 5000;
 
 const corsOptions = {
   origin: '*',
@@ -16,8 +17,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 
-// user jobIOpro
-// pass 2mJ8EIxUUNDy4F44
+
 
 
 
@@ -28,7 +28,7 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mnum3sy.mongodb.net/?retryWrites=true&w=majority`;
 
-// const uri = "mongodb+srv://jobIOpro:2mJ8EIxUUNDy4F44@cluster0.mnum3sy.mongodb.net/?retryWrites=true&w=majority";
+
 
 
 
@@ -42,6 +42,41 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+
+// my middleware
+// const logger = async (req, res, next) => {
+//   console.log('called::', req.host, req.originalUrl)
+//   next()
+// }
+
+// const verifyToken = async (req, res, next) => {
+//   const token = req.cookies?.token;
+//   console.log('value of token in middleware', token)
+//   if(!token){
+//     return res.status(401).send({message: 'Not Authorized'})
+//   }
+
+//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+//     if(err){
+//       console.log(err)
+//       return res.status(401).send({message: 'Unauthorized'})
+//     }
+
+//     console.log('value in the token', decoded)
+//     req.user = decoded
+
+//     next()
+//   })
+
+
+ 
+// }
+
+
+
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -51,8 +86,26 @@ async function run() {
     const bidcollection = client.db("jobio").collection("bids");
 
 
+    // auth related api
+    // app.post('/jwt', async (req, res) => {
+    //   const user = req.body;
+    //   console.log(user);
+    //   const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+
+
+    //   res
+    //   .cookie('token', token, {
+    //     httpOnly: true,
+    //     secure: false,
+       
+
+    //   })
+    //   .send({success: true})
+    // })
+
+
     // for jobs
-    app.get('/jobs', async (req, res) => {
+    app.get('/jobs',  async (req, res) => {
       const cursor = jobCollection.find();
       const result = await cursor.toArray();
       res.send(result);
@@ -62,6 +115,14 @@ async function run() {
     app.get ('/jobpost', async (req, res) => {
       const cursor = jobCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    // for update job
+    app.get('/jobs/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await jobCollection.findOne(query);
       res.send(result);
     })
 
@@ -86,11 +147,45 @@ async function run() {
       const result = await jobCollection.deleteOne(query);
       res.send(result);
     })
+
+    // for update
+    app.put ('/jobs/:id', async (req, res) => {
+     const id = req.params.id;
+     const filter = {_id: new ObjectId(id)};
+     const options = {upsert: true};
+     const updatedJob = req.body;
+     console.log(updatedJob)
+     const job = {
+       $set:{
+        email: updatedJob.email,
+        job: updatedJob.job,
+        deadline: updatedJob.deadline,
+        description: updatedJob.description,
+        maxprice: updatedJob.maxprice,
+        minprice: updatedJob.minprice,
+        category: updatedJob.category
+       }
+     }
+     const result = await jobCollection.updateOne(filter, job, options)
+     res.send(result)
+    
+    })
+
+
     
 
 
     // for bids
-    app.get ('/bids', async (req, res) => {
+    app.get ('/bids',   async (req, res) => {
+      console.log(req.query.email)
+      // console.log('token from client', req.cookies.token)
+      // console.log('user in the valid token', req.user)
+      // if(req.query?.email !== req.user?.email){
+      //   return res.status(403).send({message: 'Forbidden Access'})
+      // }
+
+
+
       const cursor = bidcollection.find();
       const result = await cursor.toArray();
       res.send(result);
@@ -107,6 +202,56 @@ async function run() {
       const result = await bidcollection.insertOne(bid);
       res.send(result);
     })
+
+    // for update btn
+    // app.patch('/bids/:id', async (req, res) => {
+    //   const id = req.params.id;
+    //   const filter = {_id: new ObjectId(id)};
+    //   const updatedJob = req.body;
+    //   console.log(updatedJob);
+    //   const updatedDocs = {
+    //     $set: {
+    //       status: updatedJob.status
+    //     },
+    //   }
+    //   const result = await bidcollection.updateOne(filter, updatedDocs);
+    //   res.send(result);
+    // })
+
+    app.patch('/bids/:id', async (req, res) => {
+      try {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const { status } = req.body;
+    
+        if (!status) {
+          return res.status(400).send({ error: 'Status is required in the request body.' });
+        }
+    
+        // Check if the status is either "accepted" or "rejected"
+        if (status !== 'Accept' && status !== 'Reject') {
+          return res.status(400).send({ error: 'Invalid status. It must be either "accepted" or "Rejected".' });
+        }
+    
+        const updatedDocs = {
+          $set: {
+            status: status
+          },
+        };
+    
+        const result = await bidcollection.updateOne(filter, updatedDocs);
+    
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send({ error: 'Internal Server Error' });
+      }
+    });
+
+    
+
+
+   
 
 
     // Send a ping to confirm a successful connection
@@ -134,3 +279,71 @@ app.listen(port, () => {
 
 
 
+
+
+
+
+
+
+
+
+
+// res.cookie(
+//   "token",
+//   tokenValue,
+//   {
+//       httpOnly: true,
+//       secure: process.env.NODE_ENV === "production" ? true: false,
+//       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+//   }
+// )
+
+// Removing/Deleting cookie:
+// res.clearCookie(
+//   "token",
+//   {
+//       maxAge: 0,
+//       secure: process.env.NODE_ENV === "production" ? true: false,
+//       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+//   }
+// )
+
+// res.cookie(
+//   "token",
+//   tokenValue,
+//   {
+//       httpOnly: true,
+//       secure: process.env.NODE_ENV === "production" ? true: false,
+//       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+//   }
+// )
+
+// Removing/Deleting cookie:
+// res.clearCookie(
+//   "token",
+//   {
+//       maxAge: 0,
+//       secure: process.env.NODE_ENV === "production" ? true: false,
+//       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+//   }
+// )
+
+// res.cookie(
+//   "token",
+//   tokenValue,
+//   {
+//       httpOnly: true,
+//       secure: process.env.NODE_ENV === "production" ? true: false,
+//       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+//   }
+// )
+
+// Removing/Deleting cookie:
+// res.clearCookie(
+//   "token",
+//   {
+//       maxAge: 0,
+//       secure: process.env.NODE_ENV === "production" ? true: false,
+//       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+//   }
+// )
